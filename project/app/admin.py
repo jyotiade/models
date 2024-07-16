cdc75ac9b912ccd53048e063a87ea5f52de695ad
jyoti ade <jyotiade287@gmail.com>
2024-07-16 18:31:36 +0530

    model1

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Student
 # Register your models here.
+
+#USED TO reflecte/show the model 0r class in admin panel
 admin.site.register(Student)
 
 
--- project/app/models.py
+++ project/app/models.py
@@ -9,3 +9,21 @@ class Student(models.Model):
     stu_age=models.IntegerField(null=True)
     stu_password=models.CharField(max_length=10,null=True)
     stu_enroll=models.IntegerField()
+
+    def __str__(self):
+        return self.stu_name+' '+self.stu_email  #to display name,email ,age password ,etc on the admin panel
+    
+    class Meta:
+         #=====change table name in database=============
+        db_table='Student'             
+
+        #======change table name in admin panel==============
+        verbose_name_plural='Student'  # in this it remove s from table name
+        #verbose_name='Student'   #by default django give model name with s.
+        
+        #used to order the name in ascending order according to alphabets
+        #capital letter always come first then small according to ascii code
+        ordering=['stu_name']
+        
+         #used to order the name in desending order according to alphabets
+        #ordering=['-stu_name']
